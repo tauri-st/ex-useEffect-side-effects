7aef6269e22d77908baee722f717c778e655cd4e
tauri_s <tauristclaire@gmail.com>
2023-12-19 15:44:09 -0800

    Modify onChange handler so that it is external function that the input calls on

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import { useState, useEffect } from "react";
 const App = () => {
   
   const [documentTitle, setDocumentTitle] = useState("");
+
   useEffect(() => {
     const document.title = documentTitle;
   }, [documentTitle])
+
+  const handleTitleChange = (e) => {
+    setDocumentTitle(e.target.value)
+  };
 
   return (
     <div className="App">
       <label htmlFor="title-input">
         Type in the input and look at the browser tab!
       </label>
-      <input id="title-input" type="text" maxLength={25} value={documentTitle} onChange={(e) => setDocumentTitle(e.target.value)}/>
+      <input id="title-input" type="text" maxLength={25} value={documentTitle} onChange={handleTitleChange}/>
     </div>
   );
 };
