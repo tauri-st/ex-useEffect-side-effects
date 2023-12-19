dc127bf3391916875b3f0753529b5cf2fff2e95a
tauri_s <tauristclaire@gmail.com>
2023-12-19 15:41:35 -0800

    Add value attribute to input to be documentTitle

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
       <label htmlFor="title-input">
         Type in the input and look at the browser tab!
       </label>
-      <input id="title-input" type="text" maxLength={25} onChange={(e) => setDocumentTitle(e.target.value)}/>
+      <input id="title-input" type="text" maxLength={25} value={documentTitle} onChange={(e) => setDocumentTitle(e.target.value)}/>
     </div>
   );
 };
